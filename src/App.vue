1cf6ebdd7021f2d66e026ac1c1b8ee15621b16de
Lê Hải Quý <haiquy2001@gmail.com>
2024-08-13 14:14:15 +0700

    login apple

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="width: 44.31px; height: 44.31px">
+    <vue-apple-login
+      v-if="!signedIn"
+      data-mode="logo-only"
+      data-border-radius="50"
+      type="sign in"
+      className="vue-apple-login"
+      :onSuccess="onSuccess"
+      :onFailure="onFailure"
+    ></vue-apple-login>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { ref } from "vue";
+const signedIn = ref(false);
+const onSuccess = (data) => {
+  console.log(data);
+  signedIn.value = true;
+};
+// const onSuccessLogout = () => {
+//   signedIn.value = false;
+// };
+const onFailure = (error) => {
+  console.log(error);
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.vue-apple-login {
+  width: 44.31px;
+  height: 44.31px;
+  background-color:cornflowerblue;
+  /* background: url("https://app-apsoutheast2-static.shopmy.com.au/shopmy/assets/apple_login.svg"); */
+  background-size: 44.31px;
+  cursor: pointer;
+}
+
+.vue-apple-login svg {
+  display: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueAppleLogin from "vue-apple-login";
 
-createApp(App).mount('#app')
+// createApp(App)
+//   .use(VueAppleLogin, {
+//     clientId: "ru.rustamiloveplove.service-id",
+//     scope: "name email",
+//     redirectURI: "https://rustam.iloveplove.ru/auth/apple/callback",
+//     state: "500",
+//     usePopup: true,
+//   })
+//   .mount("#app");
+
+createApp(App)
+  .use(VueAppleLogin, {
+    clientId: "s-booking.net",
+    scope: "name email",
+    redirectURI: "https://quy-1.developer.vi-jp-te.info/auth/apple/callback",
+    state: "500",
+    usePopup: true,
+  })
+  .mount("#app");
